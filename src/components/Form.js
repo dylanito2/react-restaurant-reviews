a896c53d282e298477845860c761ae75c05eec6e
dylanito2 <dylanito2@gmail.com>
2017-04-07 14:48:39 -0400

    changed some classes to consts

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
-export default class Form extends React.Component {
-  render() {
-    return (
-      <div className="col-md-6 col-md-offset-3 form-container">
-        <form onSubmit={this.props.onSubmit}>
-          <label>Restaurant Name</label><br />
-          <input type='text' value={this.props.name} name='name' onChange={this.props.onChange} className='form-control form-fields' /><br />
-          <label>Zip Code</label><br />
-          <input type='text' value={this.props.location} name='location' onChange={this.props.onChange} className='form-control form-fields' /><br />
-          <input type='submit' className='btn btn-lg btn-classic'/>
-        </form>
-      </div>
-    )
-  }
-}
+const Form = ( props ) => (
+  <div className="col-md-6 col-md-offset-3 form-container">
+    <form onSubmit={props.onSubmit}>
+      <label>Restaurant Name</label><br />
+      <input type='text' value={props.name} name='name' onChange={props.onChange} className='form-control form-fields' /><br />
+      <label>Zip Code</label><br />
+      <input type='text' value={props.location} name='location' onChange={props.onChange} className='form-control form-fields' /><br />
+      <input type='submit' className='btn btn-lg btn-classic'/>
+    </form>
+  </div>
+)
+
+export default Form
--- src/components/GifBox.js
+++ src/components/GifBox.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export default class GifBox extends React.Component {
-  render() {
-    return (
-      <div className="col-md-8 col-md-offset-2" id='showGif'>
-        <img src={this.props.gifUrl} alt='review-gif' className='giphy-box' />
-      </div>
-    )
-  }
-}
+const GifBox = ( props ) => (
+  <div className="col-md-8 col-md-offset-2" id='showGif'>
+    <img src={props.gifUrl} alt='review-gif' className='giphy-box' />
+  </div>
+)
+
+export default GifBox
